ee064fe200ee4270b3503513326ace0e49592be3
dlruawo07 <dlruawo07@gmail.com>
2023-03-30 05:13:40 +0900

    index.py, artist.py: 매번 크롤링 하는게 아닌 db에서 데이터 추출하는거로 수정 그 외에 다른 파일들 변수명 수정 / css 조금 수정

--- views/artist.py
+++ views/artist.py
@@ -1,15 +1,5 @@
 from flask import Blueprint, render_template, request
-from bs4 import BeautifulSoup
-import requests
-from selenium import webdriver
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-
-
-import time
-import json
+import pymysql
 
 
 # 루트
@@ -19,136 +9,39 @@ bp = Blueprint("artist", __name__, url_prefix="/artist")
 # /artist 경로에서
 @bp.route("/", methods=["GET"])
 def index():
+    db = pymysql.connect(
+        host="artist.cqjw1iajcspu.ap-northeast-2.rds.amazonaws.com",
+        port=3306,
+        user="admin",
+        passwd="qlgodrl12",
+        db="artist",
+        charset="utf8",
+    )
 
-    driver = webdriver.Chrome()  # 드라이버 설정
-
-    driver.implicitly_wait(10)  # 10초안에 웹페이지 로드시 바로 넘어감
+    cursor = db.cursor()
 
     artist_id_num = request.args.get("artist_id_num")
 
-    # print(artist_id_num)
+    # DB에서 request 받은 id를 찾아서 해당 정보를 딕셔너리로 바꾸어 리턴
+    cursor.execute("SELECT * FROM artists WHERE id = %s", artist_id_num)
+    row = []
 
-    default_url = "https://www.melon.com/artist/timeline.htm?artistId="
+    for x in cursor:
+        for y in x:
+            row.append(y)
 
-    url = default_url + artist_id_num 
+    for i in range(len(row)):
+        print(i, row[i])
 
-    # print(url)
+    artist = {
+        "artist_name": row[1],
+        "img_url": row[4],
+        "fan_num": row[5],
+        "members": row[6],
+        "debut": row[7],
+        "debut_song": row[8],
+        "a_type": row[9],
+        "company": row[10],
+    }
 
-    driver.get(url)
-
-    # WebDriverWait(driver, 20).until(EC.visibility_of_element_located((By.CLASS_NAME, "more")))
-
-    html =  driver.page_source
-
-    # print(html)
-
-    soup = BeautifulSoup(html, "html.parser")  # SOUP 객체로 웹사이트 변환
-
-    # print(soup)
-
-
-    wrap_info = soup.select_one("div.dtl_atist")
-
-
-    # wrap_info = soup.select_one("#wrap > #cont_wrap > #conts_section > #conts >  div.wrap_dtl_atist > div.dtl_atist clfix")
-
-    # print(wrap_info)
-
-    img_url = wrap_info.select_one("#artistImgArea > img")["src"]
-
-    # print(img_url)
-
-    artist_info = wrap_info.select_one("div.wrap_atist_info")
-
-    # print(artist_info)
-
-    artist_name = artist_info.select_one("p.title_atist").text
-
-    artist_name = artist_name[5:]
-
-    # print(artist_name)
-
-    artist_group_names = artist_info.select("a.atistname > span")
-
-
-    # print(artist_group_names[0])
-    # print(artist_group_names[1])
-    # print(artist_group_names[2])
-    # print(artist_group_names[3])
-    # print(artist_group_names[4])
-
-
-
-
-    namelist = []
-
-    for el in artist_group_names[::2]:
-        el = str(el)
-        name = el[6:-7]
-        namelist.append(name)
-        
-    # print(namelist)
-
-    artist_group_names = ', '.join(s for s in namelist)
-
-    # print(artist_group_names)
-
-    if artist_info.select_one("span.gubun") is None:
-        debut = "none"
-    else:
-        debut = artist_info.select_one("span.gubun").text
-
-# print(debut)
-
-    if artist_info.select_one("span.songname12") is None:
-        music = ""
-    else:
-        music = artist_info.select_one("span.songname12").text
-
-    # print(music)
-
-    artist_detail_info = artist_info.select_one("dl.atist_info")
-
-    # print(artist_detail_info)
-
-    info_list = artist_detail_info.select("dd")
-
-    # print("dd")
-
-    if artist_info.select_one("span.gubun") is None:
-        a_type = str(info_list[0])[4:-5]
-        company = str(info_list[1])[4:-5]
-    else:
-        a_type = str(info_list[1])[4:-5]
-        company = str(info_list[2])[4:-5]
-
-
-
-    # print(company)
-     
-    fan_num = wrap_info.select_one("#d_like_count").text
-
-    # print(fan_num)
-
-    artists = {}
-
-    artists["artist_name"] = artist_name
-    artists["artist_group_names"] = artist_group_names
-    artists["debut"] = debut
-    artists["music"] = music
-    artists["company"] = company
-    artists["img_url"] = img_url
-    artists["fan_num"] = fan_num
-    artists["a_type"] = a_type
-
-
-
-
-
-
-    # print(artists)
-
-    driver.quit()
-
-
-    return render_template("artist.html", artist_info = artists)
+    return render_template("artist.html", artist_info=artist)
--- views/index.py
+++ views/index.py
@@ -1,9 +1,6 @@
-from flask import Blueprint, render_template, jsonify, request
-from selenium import webdriver
-from bs4 import BeautifulSoup
-from selenium.webdriver.common.by import By
+from flask import Blueprint, render_template
 
-import time
+import pymysql
 import json
 
 
@@ -17,75 +14,37 @@ def home():
 
 @bp.route("/ranking", methods=["GET"])
 def index():
-    # mac일경우
+    db = pymysql.connect(
+        host="artist.cqjw1iajcspu.ap-northeast-2.rds.amazonaws.com",
+        port=3306,
+        user="admin",
+        passwd="qlgodrl12",
+        db="artist",
+        charset="utf8",
+    )
 
-    # driver = webdriver.Chrome('/Users/dlruawo07/Downloads/chromedriver_mac_arm64/chromedriver')
-
-    # window일경우
-
-    driver = webdriver.Chrome()  # 드라이버 설정
-
-    driver.implicitly_wait(10)  # 10초안에 웹페이지 로드시 바로 넘어감
-
-    driver.get("https://www.melon.com/artistplus/artistchart/index.htm")
-
-    while True:
-        try:
-            more = driver.find_element(
-                By.XPATH, '//*[@id="conts"]/div[3]/div[3]/button/span/span'
-            )
-            more.click()
-            driver.implicitly_wait(0.3)  # 0.3초안에 웹페이지 로드시 바로 넘어감
-        except:
-            break
-
-    html = driver.page_source
-
-    soup = BeautifulSoup(html, "html.parser")
+    cursor = db.cursor()
 
     artists = []
 
-    lists = soup.select(
-        "#conts > div.ltcont > div.wrap_list_artistplus.d_artist_list > ul > li"
-    )
-
-    for el in lists:
-        image_url = el.select_one("div.artistplus > div.wrap_thumb > a > img")["src"]
-        rank = el.select_one("div.artistplus_li_wrap > span.rank").text
-        wrap_info = el.select_one("div.artistplus > div.wrap_info > dl")
-        name = wrap_info.select_one("dt > a").text
-        fan = wrap_info.select_one("dd.gubun").text[2:10].strip()
-        # 고유 아티스트 넘버 
-        artist_id = wrap_info.select_one("dt > a")["href"]
-        artist_id_num = artist_id[38:-3]
-
-        # [0] : 음원 점수
-        # [1] : 팬 증가수 점수
-        # [2] : 좋아요 점수
-        # [3] : 포토 점수
-        # [4] : 비디오 점수
-        # 점수들의 평균(이 때 음원 점수는 x4, 팬 증가수 점수는 x2)을 내어 순위를 정함
-        score = (
-            wrap_info.select_one("dd.consumer_list > table > tbody > tr")
-            .text.strip()
-            .split("\n")
-        )
-
-        score = [float(x) for x in score]
-        score[0], score[1] = score[0] * 4, score[1] * 2
-
-        average_point = str(round(sum(score) / len(score) / 1.8, 2))
-
+    # DB에서 table 전체를 추출해 artists 리스트에 딕셔너리들 추가해서 리턴
+    cursor.execute("SELECT * FROM artists")
+    table = cursor.fetchall()
+    for row in table:
         artists.append(
             {
-                "rank": rank,
-                "name": name,
-                "fan": fan,
-                "average_point": average_point,
-                "image_url": image_url,
-                "artist_id_num" : artist_id_num
+                "ranking": row[0],
+                "name": row[1],
+                "fan": row[2],
+                "score": row[3],
+                "image_url": row[4],
+                "id": row[5],
+                "members": row[6],
+                "debut": row[7],
+                "debut_song": row[8],
+                "a_type": row[9],
+                "company": row[10],
             }
         )
-    driver.quit()
 
     return json.dumps(artists)
